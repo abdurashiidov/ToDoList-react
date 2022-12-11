fc21b29b6d6359dbb860e1220c448e96cae7b1b3
abdurashiidov <abdurashiidov18@gmail.com>
2022-12-11 23:05:12 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState(JSON.parse(window.localStorage.getItem("todos")) ||  [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    <h1 className='heading'>To Do List</h1>
+    
+    <input
+    className='todoInput'
+    onKeyPress={(e) =>{
+      if(e.code === "Enter"){
+
+        let newTodo = {
+          id: new Date().getTime(),
+          content: e.target.value,
+          isCompleted: false
+        }
+
+        setTodo([newTodo, ...todo])
+
+        window.localStorage.setItem("todos", JSON.stringify([newTodo, ...todo]))
+
+                e.target.value = ""
+
+      }
+    }}
+    
+    placeholder={"Malumot kiriting..."}/>
+
+    <ul>
+      {
+        todo.map(item =>{
+          return(
+            <li 
+            className='items'
+              style={{textDecoration: item.isCompleted ? "line-through" : "none"}}
+            key={item.id}>
+
+              <input 
+              defaultChecked={item.isCompleted}
+              
+              onChange={(e) => {
+                const todoId = e.target.dataset.id;
+                const findTodo = todo.find(i => i.id === Number(todoId));
+                findTodo.isCompleted = !findTodo.isCompleted
+                setTodo([...todo]);
+
+                window.localStorage.setItem("todos", JSON.stringify([...todo]))
+              }}
+
+
+              data-id = {item.id}
+              type={"checkbox"}/>
+
+              {item.content}
+
+              <button onClick={(e) =>{
+                let filteredTodo = todo.filter(i => i.id !== Number(item.id));
+
+                setTodo(filteredTodo)
+
+                window.localStorage.setItem("todos", JSON.stringify(filteredTodo))
+
+              }}>x</button>
+            </li>
+          )
+        })
+      }
+    </ul>
+
+    </>
   );
 }
 
